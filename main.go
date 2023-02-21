e895956f2376b19b2ce67910b8ae0b1f349c2274
Allan Muhari <allanmuhari087@gmail.com>
2023-02-22 01:02:02 +0300

    connecting with initializers

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"go-crud/controllers"
+
 	"go-crud/initializers"
 
 	"github.com/gin-gonic/gin"
@@ -13,10 +15,9 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
+
 	r.Run()
 }
